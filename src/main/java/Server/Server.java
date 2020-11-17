755214e5945ce6a45306e0ebc23dd9fc2b2e7f2a
Santiago Lovelli <lovelli.santiago.9@gmail.com>
2020-11-17 09:47:27 -0300

    cambio puerto

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -8,7 +8,7 @@ import spark.debug.DebugScreen;
 
 public class Server {
     public static void main(String[] args){
-        Spark.port(9000);
+        Spark.port(9001);
         Router.init();
         DebugScreen.enableDebugScreen();
     }
