7e1b48a758c50a1367e1bcc975f41446764ad9c6
Joaquin Rode <39282494+jtrode@users.noreply.github.com>
2020-12-04 21:06:55 -0300

    Constructor server

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -7,6 +7,10 @@ import spark.Spark;
 import spark.debug.DebugScreen;
 
 public class Server {
+    
+    public Server() {
+    }
+    
     public static void main(String[] args){
         Spark.port(9001);
         Router.init();
